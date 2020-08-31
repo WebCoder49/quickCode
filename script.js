a085eca308e7d010b58a40d60efb150f13bfbedf
WebCoder49 <69071853+WebCoder49@users.noreply.github.com>
2020-08-31 19:09:05 +0100

    Edited sections on different devices + files .bigButtons started

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 function toggleFiles() {
-    document.getElementById("files").classList.add("open");
+    document.getElementById("files").classList.toggle("open");
+}
+function viewFiles() {
+    document.getElementById("files").className = "open";
+    document.getElementById("code").className = "";
+    document.getElementById("result").className = "";
+    document.getElementById("filesButton").className = "open";
+    document.getElementById("codeButton").className = "";
+    document.getElementById("resultButton").className = "";
+}
+function viewCode() {
+    document.getElementById("files").className = "";
+    document.getElementById("code").className = "open";
+    document.getElementById("result").className = "";
+    document.getElementById("filesButton").className = "";
+    document.getElementById("codeButton").className = "open";
+    document.getElementById("resultButton").className = "";
+}
+function viewResult() {
+    document.getElementById("files").className = "";
+    document.getElementById("code").className = "";
+    document.getElementById("result").className = "open";
+    document.getElementById("filesButton").className = "";
+    document.getElementById("codeButton").className = "";
+    document.getElementById("resultButton").className = "open";
 }
