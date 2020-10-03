d88b5386970d80b56178b59c32ea2a10cb76ad2a
WebCoder49 <69071853+WebCoder49@users.noreply.github.com>
2020-10-03 18:48:56 +0100

    Added code + result, but not file system...

--- script.js
+++ script.js
@@ -25,3 +25,28 @@ function viewResult() {
     document.getElementById("codeButton").className = "";
     document.getElementById("resultButton").className = "open";
 }
+function highlight(code) {
+    let linen = code.split("\n").length;
+    document.getElementById("plainCode").style.height = (linen * 20) + "px";
+    document.getElementById("richCode").style.height = (linen * 20) + "px";
+    document.getElementById("richCode").innerText = code;
+    hljs.highlightBlock(document.getElementById("richCode"));
+}
+function codeKeyPressed(event) {
+    if(event.code == "Tab") {
+        event.preventDefault();
+        let tarea = document.getElementById("plainCode");
+            let oldVal = tarea.value;
+            let beginningCode = oldVal.substr(0, tarea.selectionStart);
+            let cPos = tarea.selectionStart;
+            let endCode = oldVal.substr(tarea.selectionEnd, tarea.value.length);
+            tarea.value = beginningCode + "\u00a0\u00a0\u00a0\u00a0" + endCode;
+            tarea.selectionStart = cPos + 4;
+            tarea.selectionEnd = cPos + 4;
+    }
+}
+function run() {
+    let frame = document.getElementById("resultFrame");
+    let code = document.getElementById("plainCode").value;
+    frame.src = "data:text/html;base64," + btoa(code);
+}
